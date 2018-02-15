00ef82a5cb70b0cf9aeb4a77f9cb914ac93145ba
kumarab-adm <kumarab-adm@NYCDK08349>
2018-02-15 14:10:48 -0500

    2

--- zzaafunction/Bye.cs
+++ zzaafunction/Bye.cs
@@ -28,7 +28,7 @@ namespace zzaafunction
 
             return name == null
                 ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Bye " + name + "!!! 1");
+                : req.CreateResponse(HttpStatusCode.OK, "Bye " + name + "!!! 2");
         }
     }
 }
