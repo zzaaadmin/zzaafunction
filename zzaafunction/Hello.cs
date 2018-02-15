9059a13dab0251d7c07d534ef050afb840fa4978
zzaaadmin <36480394+zzaaadmin@users.noreply.github.com>
2018-02-15 14:06:30 -0500

    Done

--- zzaafunction/Hello.cs
+++ zzaafunction/Hello.cs
@@ -28,7 +28,7 @@ namespace zzaafunction
 
             return name == null
                 ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name + "!!!");
+                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name + "!!! 1");
         }
     }
 }
