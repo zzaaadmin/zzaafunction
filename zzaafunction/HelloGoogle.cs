fba972318315af63cf527e6018252ce5f648af4c
Kumar <kumarab@wcnet.whitecase.com>
2018-03-14 16:23:56 -0400

    test

--- zzaafunction/HelloGoogle.cs
+++ zzaafunction/HelloGoogle.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -21,17 +24,16 @@ namespace zzaafunction
             
             log.Info($"Color : {data.result.parameters.color}");
             log.Info($"Number : {data.result.parameters.number}");
-            log.Info($"intentName : {data.result.intentName}");
+            log.Info($"intentName : {data.result.metadata.intentName}");
 
             string intentName = data.result.metadata.intentName;
-            string color = data.result.parameters.color;
-            string number = data.result.parameters.number;
+            string state = data.result.parameters.state;
 
             string message = "";
 
-            if(intentName == "make_name")
+            if(intentName == "get_capital")
             {
-                message = $"Your funny name is {color} {number}.";
+                message = GetStateCapital(state, log);
             }
             else
             {
@@ -42,6 +44,62 @@ namespace zzaafunction
                 speech = message,  // ASCII characters only
                 displayText = message
             });
+        }
+
+        private static string GetStateCapital(string state, TraceWriter log)
+        {
+            log.Info($"GetStateCapital:Entry");
+            string output = "";
+            string capital = "";
+
+
+            try
+            {
+                string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DatabaseConnection"].ConnectionString;
+                log.Info($"ConnectionString={connectionString}");
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("SELECT sc.State, sc.Capital ");
+                    sb.Append("FROM [dbo].[StateCapital] sc ");
+                    sb.Append("Where sc.State = '");
+                    sb.Append(state);
+                    sb.Append("';");
+                    String sql = sb.ToString();
+                    log.Info($"Query={sql}");
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                capital = reader.GetString(1);
+                            }
+                        }
+                    }
+                }
+
+                if (String.IsNullOrEmpty(capital))
+                {
+                    output = $"The capital of {state} is not configured.";
+                }
+                else
+                {
+                    output = $"The capital of {state} is {capital}.";
+                }
+            }
+            catch (SqlException ex)
+            {
+                log.Error("Error Message", ex);
+                throw;
+
+            }
+
+            log.Info($"GetStateCapital:Exit");
+            return output;
         }
     }
 }
